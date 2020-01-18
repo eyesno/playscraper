7be1d65e844c5285afc3b3a08bd266af37d97eb1
Darryl Wright <darryl@punkscience.ca>
2020-01-18 18:05:51 -0400

    Now driven by sources.json file.

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 import json
 from parsers.centerstagetheatre import parser as parserCST
 
-sources = [ { 'name': 'CentreStage Theatre',
-              'parser': 'centrestagetheatre',
-              'urls': ['https://www.centrestagetheatre.ca/index.php/now-playing',
-                       'https://www.centrestagetheatre.ca/coming-soon']
-            }  
-]
-
-
 def main():
     collection = []
 
-    for source in sources:
-        print( f"Scraping %s..." % source['name'] )
+    with open( 'sources.json') as sourceFile:
 
-        if source['parser'] == 'centrestagetheatre':
-            collection += parserCST.parseCST( source )
+        sourceData = json.load( sourceFile )
+
+        for source in sourceData['sources']:
+            print( f"Scraping %s..." % source['name'] )
+
+            if source['parser'] == 'centrestagetheatre':
+                collection += parserCST.parseCST( source )
 
     savePlays( collection )
        
     
 
 def savePlays( collection ):
-    with open( 'plays.json', 'w')  as output:
+    with open( 'output/plays.json', 'w')  as output:
         json.dump( { 'plays': collection }, output )
 
 main()
